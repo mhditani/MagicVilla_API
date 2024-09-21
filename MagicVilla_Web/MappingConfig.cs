0e7d416d16dfcc8e948cbff474c2f80b99d45aa3
mohamad itani <mohammad.adnan.itani@gmail.com>
2024-09-21 11:06:36 +0300

    Connecting

---
+++ MagicVilla_Web/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.DTO;
+
+namespace MagicVilla_Web.Mappers
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDto, VillaCreateDto>().ReverseMap();
+
+            CreateMap<VillaDto, VillaUpdateDto>().ReverseMap();
+
+
+            CreateMap<VillaNumberDto, VillaNumberCreateDto>().ReverseMap();
+            CreateMap<VillaNumberDto, VillaNumberUpdateDto>().ReverseMap();
+
+
+
+        }
+    }
+}
---
+++ MagicVilla_Web/Services/IServices/IVillaService.cs
@@ -0,0 +1,17 @@
+﻿using MagicVilla_Web.Models.DTO;
+
+namespace MagicVilla_Web.Services.IServices
+{
+    public interface IVillaService
+    {
+        Task<T> GetAllAsync<T>();
+
+        Task<T> GetAsync<T>(int id);
+
+        Task<T> CreateAsync<T>(VillaCreateDto dto);
+
+        Task<T> UpdateAsync<T>(VillaUpdateDto dto);
+
+        Task<T> DeleteAsync<T>(int id);
+    }
+}
